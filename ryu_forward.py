e7235cf0811e77b15c17d651ba2aaf37c27885d2
ZTT <65460285+ZTX1836255060@users.noreply.github.com>
2023-12-13 23:54:32 +0800

    Update ryu_forward.py

--- ryu_forward.py
+++ ryu_forward.py
@@ -20,8 +20,6 @@ class SimpleSwitch13(app_manager.RyuApp):
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self.logger.info("OFP_NO_BUFFER value: %s", ofproto_v1_3.OFP_NO_BUFFER)
-
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
@@ -29,7 +27,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # match all package
+        # match all package as Miss Table
         match = parser.OFPMatch()
 
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
@@ -62,7 +60,7 @@ class SimpleSwitch13(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         if ev.msg.msg_len < ev.msg.total_len:
-            self.logger.debug("packet truncated: only %s of %s bytes", ev.msg.msg_len, ev.msg.total_len)
+            self.logger.warn("packet truncated: only %s of %s bytes", ev.msg.msg_len, ev.msg.total_len)
 
         msg = ev.msg
         datapath = msg.datapath
@@ -82,15 +80,15 @@ class SimpleSwitch13(app_manager.RyuApp):
         dpid = format(datapath.id, "d").zfill(16)
         self.mac_to_port.setdefault(dpid, {})
    
-        # self.logger.info("PackIn Event\n"
-        #                  "  dpid: %s\n"
-        #                  "  src: %s\n"
-        #                  "  dst: %s\n"
-        #                  "  in_port: %s\n",
-        #                  dpid, eth_src, eth_dst, in_port)
+        self.logger.info("PacketIn:\n"
+                         "[dpid: %s], "
+                         "[src: %s], "
+                         "[dest: %s], "
+                         "[port_in: %s]\n",
+                         dpid, eth_src, eth_dst, in_port)
 
         self.mac_to_port[dpid][eth_src] = in_port
-        self.logger.info("mac_to_port Table %s \n", self.mac_to_port)
+        self.logger.info("[mac_to_port: %s]\n", self.mac_to_port)
 
         if eth_dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][eth_dst]
@@ -132,26 +130,14 @@ class SimpleSwitch13(app_manager.RyuApp):
         data = None
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
-
         
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port, actions=actions,
                                   data=data)
-        self.logger.info("PackOut Event\n"
-                         "  dpid=%s\n"
-                         "  in_port=%s\n"
-                         "  actions=%s\n"
-                         "  buffer_id=%s\n",
+        self.logger.info("PacketOut:\n"
+                         "[dpid: %s], "
+                         "[in_port: %s], "
+                         "[actions: %s], "
+                         "[buffer_id: %s]\n",
                          dpid, in_port, actions, msg.buffer_id)
 
-        datapath.send_msg(out)        
-        
-        
-        
-        
-        
-        
-            
-        
-    
-    
-    
+        datapath.send_msg(out)         
